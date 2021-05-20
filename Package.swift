36130981d1cb5587bc17c0722cd19dd99118b951
Insu Byeon <yuki@yuki.kr>
2021-05-20 23:22:57 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -11,7 +11,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "0.12.78")
+    .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.52")
   ],
   targets: [
     .target(
